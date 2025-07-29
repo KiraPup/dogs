e0927d8affa4b9e203e72d7f298fcd968e80f02f
Кира <gruppa1@gmail.com>
2025-07-29 18:23:20 +0300

    Создана функция showerror

--- dog www.py
+++ dog www.py
@@ -2,6 +2,28 @@ from tkinter import *
 import requests #для загрузки изображений из интернета
 from PIL import Image, ImageTk
 from io import BytesIO #для обработки изображений
+from tkinter import messagebox as mb
+
+
+def show_image():
+    image_url = get_doc_image()#ссылка на картинку с помощью гет док имадж
+    if image_url:#если ссылка не пустая
+        try:
+            response = requests.get(image_url, stream=True)#ответ будет равен запросу получаем из интернета
+            response.raise_for_status() #обрабатываем ошибки
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img.thumbnail((300,300))
+            label.config(image=img)#загружаем в метку изображение
+            label.image = img #чтобы компьютер в мусор не выкинул
+        except Exception as e:
+            mb.showerror('Ошибка',"Возникла ошибка {e}")
+
+
+
+
+
+
 
 window = Tk()
 window.title("Картинки собачки")
